26304c492a01a4fb3d6182855ecde5acfb9cbd15
theoverse <theoverse@protonmail.com>
2022-02-05 10:38:02 -0800

    added return to top feature and animated score ouput

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ form.addEventListener('submit', e => {
     })
 
     // show result on page
-    result.querySelector('span').textContent = `${score}%`
+    scrollTo(0,0)
+    
     result.classList.remove('d-none')
+
+    let output = 0
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`
+        if (output === score) {
+            clearInterval(timer)
+        } else {
+            output++
+        }
+    }, 10)
 })
