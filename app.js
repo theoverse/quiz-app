1961a410cdcae26e79459ca787040fdf4c834b73
theoverse <theoverse@protonmail.com>
2022-02-05 10:14:47 -0800

    added result section and display score feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'A', 'A', 'A', 'B']
 const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -14,5 +15,7 @@ form.addEventListener('submit', e => {
         }
     })
 
-    console.log(score)
+    // show result on page
+    result.querySelector('span').textContent = `${score}%`
+    result.classList.remove('d-none')
 })
